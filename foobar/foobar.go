c6b850573528236a5bc66661d48d3058f2b51ba6
Pallat Anchaleechamaikorn <pallat.anc@cjexpress.co.th>
2022-03-02 14:43:52 +0700

    any number wants itself as string using strconv.Itoa

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -1,17 +1,15 @@
 package foobar
 
+import "strconv"
+
 func Say(n int) string {
 	if n == 5 {
 		return "Bar"
 	}
-	if n == 4 {
-		return "4"
-	}
+
 	if n == 3 {
 		return "Foo"
 	}
-	if n == 2 {
-		return "2"
-	}
-	return "1"
+
+	return strconv.Itoa(n)
 }
